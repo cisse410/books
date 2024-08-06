eb3becd5cdc75cc87efc8d6bee0900dc5742a9c4
cisse410 <dramisse90@gmail.com>
2024-08-06 10:51:41 +0000

    Initial commit

---
+++ src/main/java/sn/cisse410/books/BooksApplication.java
@@ -0,0 +1,13 @@
+package sn.cisse410.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+}
